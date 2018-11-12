804ef3317a81a20ad7c7d2598f4c0e8653420633
mbugwasami <samimbugwa@gmail.com>
2018-11-12 08:56:42 +0300

    [Feature #161870619] Consume post user api

--- UI/js/login.js
+++ UI/js/login.js
@@ -1,9 +1,7 @@
-class Auths {
+class Login {
 
-  constructor(email, names, role, password) {
-    this.email = email
-    this.names = names
-    this.role = role
+  constructor(email, password) {
+    this.email = email,
     this.password = password
 
 
@@ -64,48 +62,7 @@ class Auths {
         document.getElementById("erroMessage").innerHTML = response.message;
       }
 
-    })
-  }
-
-  add_user(){
-
-        // url for endpoint
-        let url = "http://127.0.0.1:5000/api/v2/users/login"
-
-        // get sign up data from ui
-        let data = {
-          email : this.email,
-          names : this.names,
-          role : this.role,
-          password : this.password
-        };
-
-        // get the access_token
-        const token = localStorage.getItem('token');
-        const access_token = "Bearer " + token
-
-        // define data to be used in options section
-        let fetchData = {
-          method: 'POST',
-          headers: {
-            "Content-Type" : "application/json",
-            "Authorization" : access_token
-          },
-          body:JSON.stringify(data)
-
-        };
-
-
-        fetch(url, fetchData)
-        .then(function(response) {return response.json()})
-        .then(function(response){
-
-          if (response.status === 201){
-            alert("User created succesfully")
-          }
-
-        })
-
+    });
   }
 }
 
@@ -116,14 +73,6 @@ login.addEventListener('submit', function getTarget(e){
   e.preventDefault();
   let email = document.getElementById('email').value;
   let password = document.getElementById('password').value;
-  auth = new Auths(email, password);
+  auth = new Login(email, password);
   auth.login();
-})
-
-let create_user = document.getElementById('create_user');
-create_user.addEventListener('click', function getTarget(e){
-  e.preventDefault();
-  auth = new Auths("email", "password");
-  auth.login();
-
-})
+});
