89a39ef850b2c81478df5702dcdfb5c81cb686d8
mbugwasami <samimbugwa@gmail.com>
2018-11-12 06:51:10 +0300

    [Finishes Feature #161805161] recactor code to have classes

---
+++ UI/js/auth.js
@@ -0,0 +1,79 @@
+class Auths {
+
+  constructor(email, names, role, password) {
+    this.email = email
+    this.names = names
+    this.role = role
+    this.password = password
+
+
+  }
+
+
+  // this function is called when the litsener responds to submit eve
+  login(){
+
+
+    // url for endpoint
+    let url = "https://adc3-store-manager-api.herokuapp.com/api/v2/users/login"
+
+    // get login data from ui
+    let data = {
+      email : this.email,
+      password : this.password
+    };
+
+
+
+    // define data to be used in options section
+    let fetchData = {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body:JSON.stringify(data)
+
+    };
+
+    // use the fetch api
+    fetch(url,fetchData)
+    .then(function(response){return response.json()})
+    .then(function(response){
+
+      // store data fore use after login
+      localStorage.setItem('token',response.token);
+      localStorage.setItem('role',response.role);
+      localStorage.setItem('names',response.names);
+
+      // login the user
+      if(response.message === "wellcome "+response.names +", "+"you are loged in as "+response.role){
+
+        // login admin
+        if(response.role == "admin"){
+          window.location.href = 'UI/Admin/admin.html';
+        }
+
+        // login store attendant
+        else{
+          window.location.href = 'UI/Attendant.html';
+        }
+      }
+
+      // respond to wrong cridentials
+      else {
+        document.getElementById("erroMessage").innerHTML = response.message;
+      }
+
+    })
+  }
+
+}
+
+let login = document.getElementById('login');
+login.addEventListener('submit', function getTarget(e){
+  e.preventDefault()
+  let email = document.getElementById('email').value;
+  let password = document.getElementById('password').value;
+  auth = new Auths(email, password);
+  auth.login();
+})
