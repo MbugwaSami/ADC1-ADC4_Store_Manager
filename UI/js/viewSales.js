ac8de1d518980901eb91d33945113f5ef884e0da
mbugwasami <samimbugwa@gmail.com>
2018-11-13 10:39:11 +0300

    [Feature #161911313] Consume admin view all sales

--- UI/js/viewSales.js
+++ UI/js/viewSales.js
@@ -44,7 +44,53 @@ class  ViewSales {
         });
   }
 
+  get_all_user_sales(){
+
+    let salesTable = document.getElementById('sales_details')
+
+    // url for endpoint
+    let url = "http://127.0.0.1:5000/api/v2/sales"
+
+    const token = localStorage.getItem('token');
+    const access_token = "Bearer " + token
+
+    // define data to be used in options section
+    let fetchData = {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization" : access_token
+      }
+    };
+
+        fetch(url, fetchData)
+        .then(function(response){return response.json()})
+        .then(function(data){
+
+          let sales = data;
+          return sales.map(function(sale){
+            salesTable.innerHTML +=`
+            <tr>
+
+            <td>${sales.indexOf(sale)+1}</td>
+            <td>${sale.product_name}</td>
+            <td>${sale.quantity}</td>
+            <td>${sale.subtotal}</td>
+            <td>${sale.user_id}</td>
+
+            </tr>`;
+          });
+
+        });
+  }
+
+
+}
+sales = new ViewSales()
+if (localStorage.getItem('role') === "admin"){
+  window.onload = sales.get_all_user_sales();
 }
 
-sales = new ViewSales()
-window.onload = sales.get_user_sales();
+else {
+  window.onload = sales.get_user_sales();
+}
