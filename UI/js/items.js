4b55e337df7b2dba1ee312c821497374cb7e915f
mbugwasami <samimbugwa@gmail.com>
2018-11-23 12:34:08 +0300

    [Feature #162164174] filter all items by product name

--- UI/js/items.js
+++ UI/js/items.js
@@ -48,7 +48,71 @@ class Products {
         });
   }
 
+  search_product(){
+
+      let productsTable = document.getElementById('product_details')
+
+      let product_name = document.getElementById('product_name').value
+      // url for endpoint
+      let url = "http://127.0.0.1:5000/api/v2/products/"+product_name
+
+      const token = localStorage.getItem('token');
+      const access_token = "Bearer " + token
+
+      // define data to be used in options section
+      let fetchData = {
+        method: 'GET',
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization" : access_token
+        }
+      };
+
+          fetch(url, fetchData)
+
+          .then(function(response){return response.json()})
+          .then(function(data){
+
+
+
+            if(data.message === "product found"){
+
+              let products = data.product;
+              productsTable.innerHTML = ""
+              return products.map(function(product){
+                productsTable.innerHTML +=`
+                <tr>
+
+                <td>${products.indexOf(product)+1}</td>
+                <td>${product.product_name}</td>
+                <td>${product.product_id}</td>
+                <td>${product.description}</td>
+                <td>${product.price}</td>
+                <td>${product.stock}</td>
+                <td>${product.min_stock}</td*kj4 k>
+                <td>${product.category}</td>
+                </tr>`;
+
+
+              });
+
+            }
+            else {
+              document.getElementById("erroMessage").innerHTML = data.message;
+            }
+          });
+
+  }
+
 }
 
 product = new Products()
 window.onload = product.get_all_products();
+
+let search_product = document.getElementById('search_product')
+
+search_product.addEventListener('click', function getTarget(e){
+  e.preventDefault();
+  product1 = new Products()
+  product.search_product()
+});
